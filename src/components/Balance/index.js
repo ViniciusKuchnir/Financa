089946625ae1b4f8613199bf55c13d9c50f6ae56
Vinícius Kuchnir <vina200902@gmail.com>
2022-07-07 00:11:05 -0300

    Criação das animações da interface Home

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -5,9 +5,26 @@ import {
     StyleSheet, 
 } from 'react-native';
 
+import {MotiView} from 'moti';
+
 export default function Balance({saldo, gastos}) {
  return (
-   <View style={styles.container}>
+   <MotiView 
+    style={styles.container}
+    from={{
+        rotateX: '-100deg',
+        opacity: 0
+    }}
+    animate={{
+        rotateX: '0deg',
+        opacity: 1
+    }}
+    transition={{
+        type: 'timing',
+        delay: 300,
+        duration: 900
+    }}
+    >
         <View style={styles.item}>
             <Text style={styles.itemTitle}>Saldo</Text>
             <View style={styles.content}>
@@ -23,7 +40,7 @@ export default function Balance({saldo, gastos}) {
                 <Text style={styles.exprenses}>{gastos}</Text>
             </View>
         </View>
-   </View>
+   </MotiView>
   );
 }
 
