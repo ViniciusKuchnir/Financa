39019a38c933fd0916d816731a52f22bc39110a9
Vinícius Kuchnir <vina200902@gmail.com>
2022-07-07 00:36:27 -0300

    Criação das animações e alterações no StyleSheet da Header do App

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,20 +6,53 @@ import {
     StatusBar,
     TouchableOpacity
 } from 'react-native';
+
 import {Feather} from '@expo/vector-icons';
+import {MotiView, MotiText} from 'moti';
 
 const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64; 
 
 export default function Header({name}){
+
     return(
         <View style={styles.container}>
-            <View style={styles.content}>
-                <Text style={styles.username}>{name}</Text>
+            <MotiView 
+                style={styles.content}
+                from={{
+                    translateY: -150,
+                    opacity: 0
+                }}
+                animate={{
+                    translateY: 0,
+                    opacity: 1
+                }}
+                transition={{
+                    type: 'timing',
+                    duration: 800,
+                    delay: 300
+                }}
+            >
+                <MotiText 
+                    style={styles.username}
+                    from={{
+                        translateX: -300,
+                    }}
+                    animate={{
+                        translateX: 0,
+                    }}
+                    transition={{
+                        type: 'timing',
+                        duration: 800,
+                        delay: 800
+                    }}
+                >
+                    Olá, {name}
+                </MotiText>
 
                 <TouchableOpacity activeOpacity={0.7} style={styles.buttonUser}>
                     <Feather name="user" size={27} color='#FFF' />
                 </TouchableOpacity>
-            </View>
+            </MotiView>
         </View>
     );
 }
@@ -40,9 +73,9 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between'
     },
     username:{
-        fontSize: 18,
+        fontSize: 22,
         color: '#FFF',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
     },
     buttonUser:{
         width:44,
